9464db17d93cd49f020d2b4646d9a23c12a6dfd9
guimy <guimingyue@gmail.com>
2022-10-16 23:11:56 +0800

    add PosixSequentialFile

--- src/env.rs
+++ src/env.rs
@@ -10,10 +10,11 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use std::cell::{RefCell, RefMut};
 use std::fs::{File, OpenOptions};
-use std::io::Error;
-use crate::log_writer::{PosixWritableFile, WritableFile};
+use std::io::{Error, Read, Seek, SeekFrom, Write};
 use crate::Result;
+use crate::slice::Slice;
 
 /// posix env implementation
 
@@ -30,4 +31,141 @@ pub fn new_writable_file(filename: &str) -> Result<Box<dyn WritableFile>>{
         Err(err) => Err(crate::Error::from(err))
     }
 
+}
+
+pub trait WritableFile {
+
+    fn append(&mut self, data: &Slice) -> Result<()>;
+
+    fn flush(&mut self) -> Result<()>;
+
+    fn close(&self) -> Result<()>;
+
+    fn sync(&self) -> Result<()>;
+
+}
+
+pub trait SequentialFile {
+
+    fn read(&self, n: usize) -> Result<Box<Slice>>;
+
+    fn skip(&self, n: u64) -> Result<()>;
+}
+
+pub trait RandomAccessFile {
+
+    fn read(offset: u64, n: usize, result: &Slice, scratch: &[u8]) -> Result<()>;
+
+}
+
+pub trait FileLock {
+
+}
+
+const kWritableFileBufferSize: usize = 65536;
+
+pub struct PosixWritableFile {
+
+    // buf_[0, pos_ - 1] contains data to be written to fd_.
+    buf: Vec<u8>,
+    pos: usize,
+    file: RefCell<File>,
+
+    // True if the file's name starts with MANIFEST.
+    is_manifest: bool,
+    filename: String,
+    // The directory of filename_.
+    dirname: String
+}
+
+fn write_unbuffered(mut file: RefMut<File>, data: &[u8], size: usize) -> Result<()> {
+    let result = file.write(&data[0..size]);
+    match result {
+        Ok(write_result) => Ok(()),
+        Err(err) => Err(crate::Error::from(err))
+    }
+}
+
+impl PosixWritableFile {
+
+    pub fn new(filename: &str, file: File) -> Self {
+        PosixWritableFile {
+            pos: 0,
+            buf: vec![0; kWritableFileBufferSize],
+            file: RefCell::new(file),
+            // todo!() filename start with MANIFEST
+            filename: filename.to_string(),
+            is_manifest: false,
+            // todo!() parse dirname from filename
+            dirname: "".to_string()
+        }
+    }
+
+    fn flush_buffer(&mut self) -> Result<()>{
+        let result = write_unbuffered(self.file.borrow_mut(), self.buf.as_slice(), self.pos);
+        self.pos = 0;
+        result
+    }
+}
+
+impl WritableFile for PosixWritableFile {
+    fn append(&mut self, data: &Slice) -> Result<()> {
+        let write_data = data.data();
+        let write_size = data.size();
+        let mut write_offset = 0;
+        let copy_size = std::cmp::min(write_size, kWritableFileBufferSize - self.pos);
+        let size = self.buf.write(&write_data[..copy_size]).expect("");
+        self.pos += size;
+        write_offset += size;
+        if write_size <= write_offset {
+            return Ok(());
+        }
+
+        // Can't fit in buffer, so need to do at least one write.
+        self.flush_buffer()?;
+
+        if write_size - write_offset < kWritableFileBufferSize {
+            self.buf.write(&write_data[size..]).expect("");
+            return Ok(());
+        }
+        write_unbuffered(self.file.borrow_mut(), write_data, write_size - write_offset)
+    }
+
+    fn flush(&mut self) -> Result<()> {
+        self.flush_buffer()
+    }
+
+    fn close(&self) -> Result<()> {
+        drop(self.file.borrow_mut());
+        Ok(())
+    }
+
+    fn sync(&self) -> Result<()> {
+        self.file.borrow_mut().sync_all()?;
+        Ok(())
+    }
+}
+
+pub struct PosixSequentialFile {
+
+    file: RefCell<File>,
+
+    filename: String
+}
+
+impl SequentialFile for PosixSequentialFile {
+    
+    fn read(&self, n: usize) -> Result<Box<Slice>> {
+        // todo!() retry
+        let mut scratch = vec![0; n];
+        match self.file.borrow_mut().read(&mut scratch) {
+            Ok(_) => Ok(Box::new(Slice::from_bytes(&scratch))),
+            Err(e) => Err(crate::Error::from(e))
+        }
+    }
+
+    fn skip(&self, n: u64) -> Result<()> {
+        self.file.borrow_mut().seek(SeekFrom::Start(n as u64))?;
+        Ok(())
+    }
 }
